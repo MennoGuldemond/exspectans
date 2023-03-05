255099e43008ea9cb8fc0d5f0e39a043f9846d93
Menno Guldemond <mennoguldemond@live.nl>
2023-03-05 19:07:49 +0100

    add tile outline on hover

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public int WorldWidth = 10;
     public int WorldHeight = 10;
+    public GameObject Outline;
 
     private WorldRenderer _worldRenderer;
     private Tilemap _tilemap;
+
+    private Vector3Int previousMousePos = new();
 
     void Start()
     {
@@ -20,16 +23,30 @@ public class GameManager : MonoBehaviour
         _tilemap = _worldRenderer.Render(world, WorldWidth, WorldHeight);
     }
 
+    private void Update()
+    {
+        var mousePos = GetMousePosition();
+        if (!mousePos.Equals(previousMousePos))
+        {
+            Outline.transform.position = new Vector3(mousePos.x + .5f, mousePos.y + .5f, 0);
+            previousMousePos = mousePos;
+        }
+    }
+
     public void OnPrimary()
     {
-        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        var tilePos = _tilemap.WorldToCell(worldPoint);
-        var tile = _tilemap.GetTile(tilePos);
+        var mousePos = GetMousePosition();
+        var tile = _tilemap.GetTile(mousePos);
 
         if (tile)
         {
-            Debug.Log($"Tile found at {tilePos}");
+            Debug.Log($"Tile found at {mousePos}");
         }
+    }
+
+    private Vector3Int GetMousePosition()
+    {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return _tilemap.WorldToCell(mouseWorldPos);
     }
 }
