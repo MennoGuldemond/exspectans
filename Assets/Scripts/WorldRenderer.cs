eb48d165e496578cd2b33cba2a9a0dd1bdad51d2
Menno Guldemond <mennoguldemond@live.nl>
2023-03-05 14:27:31 +0100

    add mouse input test for tiles

--- Assets/Scripts/WorldRenderer.cs
+++ Assets/Scripts/WorldRenderer.cs
@@ -15,6 +15,8 @@ public class WorldRenderer : MonoBehaviour
     private TilemapRenderer _tilemapRenderer;
     private Tilemap _tilemap;
 
+    private Vector2 _worldPoint;
+
     void Start()
     {
         _tilemapRenderer = GetComponentInChildren<TilemapRenderer>();
@@ -23,6 +25,22 @@ public class WorldRenderer : MonoBehaviour
         var worldGenerator = new WorldGenerator();
         var world = worldGenerator.Generate(Width, Height);
         Render(world);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            var tilePos = _tilemap.WorldToCell(_worldPoint);
+            var tile = _tilemap.GetTile(tilePos);
+
+            if (tile)
+            {
+                Debug.Log($"Tile found at {tilePos}");
+            }
+        }
     }
 
     void Render(World world)
