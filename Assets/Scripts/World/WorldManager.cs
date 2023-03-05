efbf754912517b0d80d0046347af322bcf0f4979
Menno Guldemond <mennoguldemond@live.nl>
2023-03-05 21:04:24 +0100

    add namespace

--- Assets/Scripts/World/WorldManager.cs
+++ Assets/Scripts/World/WorldManager.cs
@@ -1,27 +1,29 @@
 using Exspectans.Data;
 using Exspectans.DependencyInjection;
-using Exspectans.World;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class WorldManager : MonoBehaviour
+namespace Exspectans.World
 {
-    public WorldData WorldData { get; private set; }
-    public Tilemap Tilemap { get; private set; }
-
-    private WorldGenerator _worldGenerator;
-    private WorldRenderer _worldRenderer;
-
-    void Start()
+    public class WorldManager : MonoBehaviour
     {
-        _worldGenerator = DependenciesContext.Dependencies.Get<WorldGenerator>();
-        _worldRenderer = DependenciesContext.Dependencies.Get<WorldRenderer>();
-    }
+        public WorldData WorldData { get; private set; }
+        public Tilemap Tilemap { get; private set; }
 
-    public Tilemap Create(int width, int height)
-    {
-        WorldData = _worldGenerator.Generate(width, height);
-        Tilemap = _worldRenderer.Render(WorldData);
-        return Tilemap;
+        private WorldGenerator _worldGenerator;
+        private WorldRenderer _worldRenderer;
+
+        void Start()
+        {
+            _worldGenerator = DependenciesContext.Dependencies.Get<WorldGenerator>();
+            _worldRenderer = DependenciesContext.Dependencies.Get<WorldRenderer>();
+        }
+
+        public Tilemap Create(int width, int height)
+        {
+            WorldData = _worldGenerator.Generate(width, height);
+            Tilemap = _worldRenderer.Render(WorldData);
+            return Tilemap;
+        }
     }
 }
