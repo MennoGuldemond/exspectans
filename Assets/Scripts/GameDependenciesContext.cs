996e1f47057887a5204f1f408555257cdc192ba2
Menno Guldemond <mennoguldemond@live.nl>
2023-03-06 14:41:19 +0100

    level rename in dependencies

--- Assets/Scripts/GameDependenciesContext.cs
+++ Assets/Scripts/GameDependenciesContext.cs
@@ -11,11 +11,11 @@ namespace Exspectans
         [SerializeField]
         private TileManager _tileManager;
         [SerializeField]
-        private LevelManager _worldManager;
+        private LevelManager _levelManager;
         [SerializeField]
-        private LevelGenerator _worldGenerator;
+        private LevelGenerator _levelGenerator;
         [SerializeField]
-        private LevelRenderer _worldRenderer;
+        private LevelRenderer _levelRenderer;
 
         private void Awake()
         {
@@ -23,9 +23,9 @@ namespace Exspectans
 
             DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(TileManager), Factory = () => _tileManager, IsSingleton = true });
             DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(GameManager), Factory = () => _gameManager, IsSingleton = true });
-            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelManager), Factory = () => _worldManager, IsSingleton = true });
-            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelGenerator), Factory = () => _worldGenerator, IsSingleton = true });
-            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelRenderer), Factory = () => _worldRenderer, IsSingleton = true });
+            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelManager), Factory = () => _levelManager, IsSingleton = true });
+            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelGenerator), Factory = () => _levelGenerator, IsSingleton = true });
+            DependenciesContext.Dependencies.Add(new Dependency { Type = typeof(LevelRenderer), Factory = () => _levelRenderer, IsSingleton = true });
         }
     }
 }
